f7eddf8abf5a777ca71e5adf7ec0adff3f4f9efd
proproductphotoru-rgb <proproductphoto.ru@gmail.com>
2026-01-20 12:01:37 +0300

    Refactor service worker for caching and activation

--- sw.js
+++ sw.js
@@ -1,10 +1,43 @@
-self.addEventListener('install',e=>{
-  e.waitUntil(
-    caches.open('app').then(c=>c.addAll(['./','./index.html']))
-  )
-})
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  )
-})
+const CACHE_NAME = 'driver-app-v1';
+
+// Файлы для кэширования
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/images/icon-192.png',
+  '/images/icon-512.png'
+];
+
+// Установка (кэширование)
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Активация (очистка старых кэшей)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Обработка запросов
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
